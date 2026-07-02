a0b91782ddc1767ebe4e200278193c7eb36044c9
Ian Kincaid <i.s.kincaid@gmail.com>
2026-07-02 13:59:03 -0400

    added NoteService method to convert and save markdown file. Saving file not yet implemented

--- src/main/java/app/markdownotes/service/NoteService.java
+++ src/main/java/app/markdownotes/service/NoteService.java
@@ -1,4 +1,33 @@
 package app.markdownotes.service;
 
+import app.markdownotes.utility.text.MarkdownConverter;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.stream.Stream;
+
+/**
+ * Service class for Notes. Calls static converter and stores formatted notes via repository.
+ */
+@ApplicationScoped
 public class NoteService {
+
+    /**
+     * Converts .txt file to .md and formats text. Saves markdown notes via repository.
+     * @param uploadedFile txt file to be converted to md
+     * @return text formatted for md
+     * @throws IOException
+     */
+    public String convertAndSaveNote(File uploadedFile) throws IOException {
+        String markdownText;
+
+        try(Stream<String> lines = Files.lines(uploadedFile.toPath())) {
+            markdownText = MarkdownConverter.convertToMD(lines);
+        }
+
+        // TODO: Save file to google cloud storage and url to database
+        return markdownText; // return formatted text for now
+    }
 }
